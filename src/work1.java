779f4f5af71b7d9962f2af05f75f723a49eb480c
drsaurabh73 <drsaurabh73@gmail.com>
2022-03-07 22:11:12 +0000

    cucumber1

---
+++ src/work1.java
@@ -0,0 +1,7 @@
+public class work1 {
+    public static void main (String [] args){
+        System.out.println("Sachin patil");
+        System.out.println("Kavpil Magan");
+        System.out.println("Azar Kaur");
+    }
+}
---
+++ src/work2.java
@@ -0,0 +1,9 @@
+public class work2 {
+    public static void main (String [] args){
+
+        System.out.println("Mr.Satnam Patil");
+        System.out.println("Mountford Street");
+        System.out.println("     London");
+        System.out.println("     W2 5LL");
+    }
+}
